c9781d94a8a4861808761b38e16df5243058cc68
Ranrovel <ranrovel1611@protonmail.com>
2023-08-07 00:45:32 +0530

    Restructure

--- app/main.py
+++ app/main.py
@@ -1,9 +1,96 @@
 import os
+import json
+import pytube
+from pathlib import Path
+from spotipy import Spotify
+from googleapiclient.discovery import build
+from spotipy.oauth2 import SpotifyClientCredentials
 
-if __name__ == "__main__":
-    os.system("python app\\spotify_playlist.py")
-    print("Fetching tracks from playlist")
-    os.system("python app\\spotify_playlist.py")
-    print("Dowloading tracks")
-    os.system("python app\\yt_links.py")
-    print("Download successful")
+# Youtube Data Api info
+service_name = "youtube"
+api_version = "v3"
+
+# Accessing credentials for Spotify and Youtube
+with open(r"..\assets\credentials.json") as file:
+    data = json.load(file)
+    spotify_client_id, spotify_client_secret = data["Spotify"].values()
+    developer_key = data["Youtube"]["developer_key"]
+    file.close()
+
+# Connecting with spotify api
+auth = SpotifyClientCredentials(spotify_client_id, spotify_client_secret)
+sp = Spotify(auth_manager=auth)
+
+# Connecting with youtube api
+yt = build(service_name, api_version, developerKey=developer_key)
+
+# Playlist link
+playlist_link = (
+    "https://open.spotify.com/playlist/3ua7cas0riaebaixijDaoq?si=1dedb4f6e6e04ff4"
+)
+# playlist_link = (
+#     "https://open.spotify.com/playlist/29CDHardEjqjwFkaTIXH5d?si=c5e69af3a38c43f0"
+# )
+playlist_data = sp.playlist(playlist_link)
+
+# Storing data
+playlist_name = playlist_data["name"]
+print(f"Fetching info from your playlist: {playlist_name}")
+total_tracks = playlist_data["tracks"]["total"]
+tracks = playlist_data["tracks"]
+spotify_items = tracks["items"]
+
+track_list = []
+artist_list = []
+track_artist_list = []
+offset = 0
+for i in range(total_tracks):
+    track_artist_list.append(
+        [
+            spotify_items[i - offset]["track"]["name"],
+            spotify_items[i - offset]["track"]["artists"][0]["name"],
+        ]
+    )
+    if (i + 1) % 100 == 0:
+        tracks = sp.next(tracks)
+        spotify_items = tracks["items"]
+        offset = i + 1
+
+# List of queries
+query_list = set(
+    filter(
+        lambda x: x.strip() not in [None, ""],
+        list(map(lambda x: " ".join(x), track_artist_list)),
+    )
+)
+
+# Download path
+path_to_download_folder = os.path.join(Path.home(), "Downloads") + "\\" + playlist_name
+print(f"Downloading your favorites songs at: {path_to_download_folder}")
+# Fetching Links and downloading files
+for query in query_list:
+    # Fecthing Links
+    """Using Pytube"""
+    s = pytube.Search(query)
+    video_id = s.results[0].video_id
+    """ Using Youtube Data API """
+    # request = yt.search().list(
+    #     part="snippet",
+    #     q=query,
+    #     maxResults=1,
+    #     order="viewCount",
+    #     type="video",
+    # )
+    # response = request.execute()
+    # yt_items = response["items"][0]
+    # video_id = yt_items["id"]["videoId"]
+    link = f"https://www.youtube.com/watch?v={video_id}"
+
+    # Downloading tracks
+    link_data = pytube.YouTube(link)
+    audio = link_data.streams.filter(adaptive=True, only_audio=True).order_by("abr")
+    audio[-1].download(path_to_download_folder, f"{query}.mp3")
+    print(f"Downloaded -> {query}")
+
+print("Opening your folder ...")
+os.startfile(path_to_download_folder)
