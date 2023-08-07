9448463514e9ffea3919ec97e18599b47b59efa3
Ranrovel <ranrovel1611@protonmail.com>
2023-08-08 01:04:17 +0530

    Paraphrase, clean name, exception handling

--- result.py
+++ result.py
@@ -1,5 +1,7 @@
 # Libraries
 import os
+import re
+import eyed3
 import pytube
 import webbrowser
 from pathlib import Path
@@ -7,8 +9,6 @@ from spotipy import Spotify
 from spotipy.oauth2 import SpotifyClientCredentials
 from dotenv import load_dotenv
 from googleapiclient.discovery import build
-
-import main
 
 # Connecting with spotify api
 load_dotenv()
@@ -26,9 +26,10 @@ yt = build(
 )
 
 # Playlist link
-playlist_link = (
-    "https://open.spotify.com/playlist/3ua7cas0riaebaixijDaoq?si=1dedb4f6e6e04ff4"
-)
+playlist_link = input("Enter Playlist link : ")
+# playlist_link = (
+#     "https://open.spotify.com/playlist/3ua7cas0riaebaixijDaoq?si=1dedb4f6e6e04ff4"
+# )
 # playlist_link = (
 #     "https://open.spotify.com/playlist/48R8WFVbjl8rO1iFfr9Hxs?si=a27673c96bb14ed1"
 # )
@@ -63,6 +64,12 @@ track_artist_list = list(
     )
 )
 
+total_not_found_tracks = total_tracks - len(track_artist_list)
+total_found_tracks = len(track_artist_list)
+print(
+    f"Playlist Name: {playlist_name} \nTotal Tracks: {total_tracks}\nFound: {total_found_tracks}\nNot Found: {total_not_found_tracks}"
+)
+
 # List of queries
 query_list = list(map(lambda x: " ".join(x[::-1]), track_artist_list))
 
@@ -70,33 +77,57 @@ query_list = list(map(lambda x: " ".join(x[::-1]), track_artist_list))
 path_to_download_folder = os.path.join(Path.home(), "Downloads") + "\\" + playlist_name
 print(f"Downloading your favorites songs at: {path_to_download_folder}")
 
+track_no = 1
+total_failed_tracks = 0
+total_downloaded_tracks = 0
 # Fetching Links and downloading files
 for i, query in enumerate(query_list):
-    # Fetching Links
-    # Using Pytube
-    s = pytube.Search(query)
-    track_id = s.results[0].video_id
+    try:
+        # Fetching Links
+        # Using Pytube
+        s = pytube.Search(query)
+        track_id = s.results[0].video_id
 
-    # Using Youtube Data API
-    # request = yt.search().list(
-    #     part="snippet",
-    #     q=query,
-    #     maxResults=1,
-    #     order="viewCount",
-    #     type="video",
-    # )
-    # response = request.execute()
-    # yt_items = response["items"][0]
-    # track_id = yt_items["id"]["videoId"]
-    link = f"https://www.youtube.com/watch?v={track_id}"
+        # Using Youtube Data API
+        # request = yt.search().list(
+        #     part="snippet",
+        #     q=query,
+        #     maxResults=1,
+        #     order="viewCount",
+        #     type="video",
+        # )
+        # response = request.execute()
+        # yt_items = response["items"][0]
+        # track_id = yt_items["id"]["videoId"]
+        link = f"https://www.youtube.com/watch?v={track_id}"
 
-    # Downloading tracks
-    link_data = pytube.YouTube(link)
-    audio = link_data.streams.filter(only_audio=True).last()
-    track_name = " by ".join(track_artist_list[i])
-    audio.download(path_to_download_folder, f"{track_name}.mp3")
-    print(f"Downloaded -> {track_name}")
+        # Downloading tracks
+        link_data = pytube.YouTube(link)
+        audio = link_data.streams.filter(only_audio=True).last()
+        track_name = " by ".join(track_artist_list[i])
+        clean_track_name = re.sub(r'["\/:?<>|]', "", track_name)
+        audio.download(path_to_download_folder, f"{clean_track_name}.mp3", timeout=15)
+        print(f"{track_no}] Downloaded -> {clean_track_name}")
+
+        # Meta data update
+        # track = eyed3.load("song.mp3")
+        # track.tag.track_num = track_no
+        # track.tag.artist = "Token Entry"
+        # track.tag.album = "Free For All Comp LP"
+        # track.tag.album_artist = "Various Artists"
+        # track.tag.title = "The Edge"
+
+        total_downloaded_tracks += 1
+        track_no += 1
+    except Exception as error_log:
+        total_failed_tracks += 1
+        print(f"Failed -> {clean_track_name}")
+        print(error_log)
 
 # Open Folder
-print("Opening your folder ...")
-webbrowser.open(path_to_download_folder)
+if total_downloaded_tracks > 0:
+    print(
+        f"Total Downloaded Tracks: {total_downloaded_tracks}\nTotal Failed Tracks: {total_failed_tracks}"
+    )
+    print(f"Opening your folder {playlist_name}")
+    webbrowser.open(path_to_download_folder)
