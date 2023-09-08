1aa0d4897dfa1f0880ee65d946283d1ce84c03ad
Gaurav-py <gamma11@solverbee.com>
2023-09-09 00:04:22 +0530

    UI changes
    Added Folder path, chk box, color changes

--- main.py
+++ main.py
@@ -1,21 +1,71 @@
 from flet import *
+import os
+import re
+import pytube
+import webbrowser
+from pathlib import Path
+from spotipy import Spotify
+from spotipy.oauth2 import SpotifyClientCredentials
+from dotenv import load_dotenv
+from googleapiclient.discovery import build
+
+
+def spotify_download(link, path):
+    pass
 
 
 def main(page: Page):
-    def show_link(a):
-        link = new_task.value
-        return link
+    def send_info(a):
+        link, path = playlist_link.value, folder_path.value
+        spotify_download(link, path)
 
     page.title = "Spotify Scraper"
-    new_task = TextField(hint_text="Playlist Link", width=500)
+    page.horizontal_alignment = CrossAxisAlignment.CENTER
+    page.vertical_alignment = MainAxisAlignment.CENTER
+    page.bgcolor = "#0B0B0D"
+
+    playlist_link = TextField(
+        hint_text="Playlist Link",
+        width=500,
+        bgcolor="#474A56",
+        border_width=2,
+        border_color="#929AAB",
+        color="#D3D5FD",
+        autofocus=True,
+    )
+    folder_path = TextField(
+        hint_text="Folder Path",
+        width=500,
+        bgcolor="#474A56",
+        border_width=2,
+        border_color="#929AAB",
+        color="#D3D5FD",
+    )
     page.add(
+        Column(
+            [
+                playlist_link,
+                folder_path,
+            ],
+            spacing=20,
+            alignment=CrossAxisAlignment.CENTER,
+        ),
         Row(
             [
-                new_task,
-                ElevatedButton("Download", on_click=show_link),
+                Checkbox(
+                    label="Change Download Location ?",
+                    value=False,
+                ),
+                ElevatedButton(
+                    "Download",
+                    height=40,
+                    width=125,
+                    on_click=send_info,
+                ),
             ],
+            spacing=50,
             alignment=MainAxisAlignment.CENTER,
-        )
+        ),
     )
 
 
