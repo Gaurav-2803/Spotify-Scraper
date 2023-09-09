ef1c201bf4426256c1c868fad0fd0407783b9cc8
Gaurav-2803 <ranrovel1611@protonmail.com>
2023-09-09 13:51:10 +0530

    UI changes
    Heading, chk_bok event

--- main.py
+++ main.py
@@ -1,13 +1,4 @@
 from flet import *
-import os
-import re
-import pytube
-import webbrowser
-from pathlib import Path
-from spotipy import Spotify
-from spotipy.oauth2 import SpotifyClientCredentials
-from dotenv import load_dotenv
-from googleapiclient.discovery import build
 
 
 def spotify_download(link, path):
@@ -15,6 +6,10 @@ def spotify_download(link, path):
 
 
 def main(page: Page):
+    def change_path_status(a):
+        folder_path.disabled = folder_path.disabled == False
+        page.update()
+
     def send_info(a):
         link, path = playlist_link.value, folder_path.value
         spotify_download(link, path)
@@ -23,7 +18,13 @@ def main(page: Page):
     page.horizontal_alignment = CrossAxisAlignment.CENTER
     page.vertical_alignment = MainAxisAlignment.CENTER
     page.bgcolor = "#0B0B0D"
-
+    # page.theme = Theme(color_scheme=ColorScheme(secondary="#D3D5FD"))
+    heading = Text(
+        value="Spotify Scraper",
+        size=60,
+        height=90,
+        color="#D3D5FD",
+    )
     playlist_link = TextField(
         hint_text="Playlist Link",
         width=500,
@@ -34,35 +35,48 @@ def main(page: Page):
         autofocus=True,
     )
     folder_path = TextField(
-        hint_text="Folder Path",
+        hint_text="Folder Path = Downloads",
         width=500,
         bgcolor="#474A56",
         border_width=2,
         border_color="#929AAB",
         color="#D3D5FD",
+        disabled=True,
+    )
+    path_change_chk = Checkbox(
+        label="Change Download Location?",
+        value=False,
+        fill_color="#D3D5FD",
+        on_change=change_path_status,
+    )
+    download_btn = ElevatedButton(
+        text="Download",
+        height=30,
+        width=115,
+        on_click=send_info,
     )
     page.add(
+        Column(
+            [
+                heading,
+            ],
+            height=100,
+            alignment=CrossAxisAlignment.CENTER,
+        ),
         Column(
             [
                 playlist_link,
                 folder_path,
             ],
-            spacing=20,
+            spacing=15,
             alignment=CrossAxisAlignment.CENTER,
         ),
         Row(
             [
-                Checkbox(
-                    label="Change Download Location ?",
-                    value=False,
-                ),
-                ElevatedButton(
-                    "Download",
-                    height=40,
-                    width=125,
-                    on_click=send_info,
-                ),
+                path_change_chk,
+                download_btn,
             ],
+            height=40,
             spacing=50,
             alignment=MainAxisAlignment.CENTER,
         ),
